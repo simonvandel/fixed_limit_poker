a2810e8e421a836c2a02460bfcbb5d26e979ed64
Troels Krøgh <troels.kroegh@gmail.com>
2021-10-03 01:02:17 +0200

    MirrorBot!

--- bots/__init__.py
+++ bots/__init__.py
@@ -2,5 +2,6 @@ from .CallBot import CallBot
 from .EmptyBot  import EmptyBot
 from .Example1Bot import Example1Bot
 from .FoldBot import FoldBot
+from .MirrorBot import MirrorBot
 from .RandomBot import RandomBot
 from .UnitTesterBot import UnitTesterBot
