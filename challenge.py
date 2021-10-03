b0995bc35059a48921dc4b48c5a53a93a723588e
bovle <frederik.bl@live.dk>
2021-10-03 21:16:20 +0200

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from json.encoder import JSONEncoder
 import pickle
+import queue
 from typing import Dict
 from environment.FixedLimitPoker import FixedLimitPoker
 from bots import Example1Bot, MirrorBot, RandomBot, CallBot, FoldBot
@@ -8,8 +9,9 @@ import itertools
 import math
 import time
 import json
+import multiprocessing as mp
 
-PARTICIPANTS = [Example1Bot(), MirrorBot(), RandomBot(), CallBot(), FoldBot()]
+PARTICIPANTS = [Example1Bot("player1"), Example1Bot("player2"), Example1Bot("player3"), Example1Bot("player4"), Example1Bot("player5")]
 TOTAL_ROUNDS = 1000
 
 
@@ -29,6 +31,26 @@ class ChallengeResultEncoder(JSONEncoder):
         return o.__dict__
 
 
+def play(jobQueue: mp.Queue, roundsPerRoom: int, stats):
+    while not jobQueue.empty():
+        try:
+            c = jobQueue.get(block=False)
+        except queue.Empty:
+            break
+        room = FixedLimitPoker(c)
+        for _ in range(roundsPerRoom):
+            room.reset(rotatePlayers=True)
+            p1 = room.players[0]
+            p2 = room.players[1]
+            k1 = (p1.bot.name, p2.bot.name)
+            if k1 not in stats:
+                stats[k1] = 0
+            stats[k1] += p1.reward
+            k2 = (p2.bot.name, p1.bot.name)
+            if k2 not in stats:
+                stats[k2] = 0
+            stats[k2] += p2.reward
+
 def main():
     combinations = list(itertools.combinations(PARTICIPANTS, 2))
     rounds_for_each_pair = math.floor(TOTAL_ROUNDS / len(combinations))
@@ -36,32 +58,40 @@ def main():
     print(f"There are {len(combinations)} combinations")
     print(f"Each combination will be played: {rounds_for_each_pair} times")
 
-    poker_rooms = [FixedLimitPoker(c) for c in combinations]
-    stats = defaultdict(lambda: 0)
-
     start_time = time.time()
-    for room in poker_rooms:
-        print(f"Pairing {' vs. '.join([p.bot.name for p in room.players])}")
-        for _ in range(rounds_for_each_pair):
-            room.reset(rotatePlayers=True)
-            for p in room.players:
-                stats[p.bot.name] += p.reward
+    manager = mp.Manager()
+    stats = manager.dict()
+    jobs = mp.Queue()
+    for c in combinations:
+        jobs.put(c)
 
-        print(json.dumps(stats, ensure_ascii=True, sort_keys=True))
+    processes = []
+    for _ in range(mp.cpu_count() - 2):
+        p = mp.Process(target=play, args=(jobs,rounds_for_each_pair,stats))
+        processes.append(p)
+        p.start()
 
+    for p in processes:
+        p.join()
+
+    res = defaultdict(dict)
+    for key in stats.keys():
+        res[key[0]][key[1]] = stats[key]
+    
     duration = time.time() - start_time
     rounds = rounds_for_each_pair * len(combinations)
     duration_pr_sim = round(duration/rounds, 5)
     print(f"-----------------------------------------")
     print(f"Simulation took {duration_pr_sim} seconds pr. round")
     print(f"--- {round(duration, 2)} seconds ---")
-
+    
     timestamp = round(time.time())
     with open(f"./results/challenge-{timestamp}-{'-'.join(p.name for p in PARTICIPANTS)}.pckl", 'wb') as file:
-        challenge_result = ChallengeResult(stats, timestamp, rounds)
+        challenge_result = ChallengeResult(res, timestamp, rounds)
         results_as_json = json.dumps(
             challenge_result, cls=ChallengeResultEncoder)
         pickle.dump(results_as_json, file)
 
 
-main()
+if __name__ == '__main__':
+    main()
