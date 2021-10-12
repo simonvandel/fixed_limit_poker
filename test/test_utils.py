e5cc240782e790766a1e8d4060753765e2f70548
bovle <frederik.bl@live.dk>
2021-10-12 19:30:52 +0200

    test virker ikke

---
+++ test/test_utils.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from utils import handValue
+
+class TestUtils(TestCase):
+
+    def testPercentValueAces(self):
+        hand = ['As', 'Ah']
+        val = handValue.getHandPercent(hand)
+        self.assertTrue(val < 1)
+
+    def test2(self):
+        self.fail()
