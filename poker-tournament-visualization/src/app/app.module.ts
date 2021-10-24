df8bd580784d3c552a5115dc7f320c1f4fa224d5
Texmus <51411619+Texmus@users.noreply.github.com>
2021-10-24 21:13:01 +0200

    tilføj chip og chipstack. Ikke skalleret ordentlig

--- poker-tournament-visualization/src/app/app.module.ts
+++ poker-tournament-visualization/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import { PokerGameModule } from './poker-game/poker-game.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import { PokerActionComponent } from './poker-action/poker-action.component';
-import { CardGroupComponent } from './card-group/card-group.component';
 
 @NgModule({
   declarations: [
